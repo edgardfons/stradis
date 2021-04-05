7c321b200dd6a2143c0ca611d2d29d4d31077ad4
Edgard Fonseca <edgard.oliveira@aluno.uece.br>
2021-04-05 20:25:01 -0300

    Estado inicial dos templates

--- app.py
+++ app.py
@@ -12,17 +12,11 @@ app.secret_key = os.getenv('SECRET_KEY', 'secret string')
 def index():
     return render_template('index.html')
 
-
 @app.route('/professores', methods=['GET', 'POST'])
 def professores():
     if request.method == 'POST':
         return redirect(url_for('index'))
-    return render_template('professores/index.html')
-
-@app.route('/professores/novo')
-def professores_novo():
-    return render_template('professores/new.html')
-
+    return render_template('professores/index.html', prof_tab=True)
 
 @app.route('/turmas', methods=['GET', 'POST'])
 def turmas():
@@ -30,19 +24,11 @@ def turmas():
         return redirect(url_for('index'))
     return render_template('turmas/index.html')
 
-@app.route('/turmas/novo')
-def turmas_novo():
-    return render_template('turmas/new.html', title="Turmas")
-
 @app.route('/salas', methods=['GET', 'POST'])
 def salas():
     if request.method == 'POST':
         return redirect(url_for('index'))
     return render_template('salas/index.html')
-
-@app.route('/salas/novo')
-def salas_novo():
-    return render_template('salas/new.html')
 
 @app.route('/cursos', methods=['GET', 'POST'])
 def cursos():
@@ -50,16 +36,8 @@ def cursos():
         return redirect(url_for('index'))
     return render_template('cursos/index.html')
 
-@app.route('/cursos/novo')
-def cursos_novo():
-    return render_template('cursos/new.html')
-
 @app.route('/disciplinas', methods=['GET', 'POST'])
 def disciplinas():
     if request.method == 'POST':
         return redirect(url_for('index'))
     return render_template('disciplinas/index.html')
-
-@app.route('/disciplinas/novo')
-def disciplinas_novo():
-    return render_template('disciplinas/new.html')
